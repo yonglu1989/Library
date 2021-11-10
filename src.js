92aaa6d86f919d9a7462a252097a6161ee1ad2b1
Yonglu <yonglu777@gmail.com>
2021-11-10 13:05:15 -0800

    Basic Styling of the form completed. Added popup Modal form. Still needs to be hooked up to JS.

--- src.js
+++ src.js
@@ -1,3 +1,5 @@
+// Initializations
+const cardsContainer = document.querySelector(".cards-container");
 let myLibrary = [];
 
 function book(title, author, pages, read) {
@@ -17,9 +19,41 @@ function book(title, author, pages, read) {
     };
 }
 
-function addBookToLibrary(bookInput) {
-    myLibrary.push(bookInput);
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new book(title, author, pages, read);
+    myLibrary.push(newBook);
 }
 
+function displayBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        let card = document.createElement("div");
+        let cardTitle = document.createElement("div");
+        let cardAuthor = document.createElement("div");
+        let cardPages = document.createElement("div");
+        let cardRead = document.createElement("div");
+
+        cardTitle.textContent = "Title: " + myLibrary[i].title.toString();
+        cardAuthor.textContent = "Author: " + myLibrary[i].author.toString();
+        cardPages.textContent = "Pages: " + myLibrary[i].pages.toString();
+        cardRead.textContent = "Read: " + myLibrary[i].read.toString();
+
+        cardTitle.classList.add('title-info');
+        cardAuthor.classList.add('author-info');
+        cardPages.classList.add('pages-info');
+        cardRead.classList.add('read-info');
+        card.classList.add('card');
+
+        card.appendChild(cardTitle);
+        card.appendChild(cardAuthor);
+        card.appendChild(cardPages);
+        card.appendChild(cardRead);
+        cardsContainer.appendChild(card);
+
+    }
+}
 // Manually creating some books for now.
-// 
+addBookToLibrary("Legends of Smithy Jones", "Pen Island", 255, true);
+addBookToLibrary("Legends of King Arthur", "Pen Island", 255, true);
+addBookToLibrary("Legends of Grey Hound", "Pen Island", 255, true);
+
+displayBooks();
